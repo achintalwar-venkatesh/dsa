f1c2a9fc7f940885f61bece24813c402269788bc
achintalwar-venkatesh <venkatesh.achintalwar@gmail.com>
2025-05-12 11:05:58 +0000

    Removed unnecessary variable.

--- recursion/sorted_array.py
+++ recursion/sorted_array.py
@@ -2,14 +2,14 @@
 
 # p -> array is sorted, sp - >
 
-nums = [1,0]
+nums = [1,10,23]
 
-def is_sorted(start,end,nums):
+def is_sorted(start,nums):
 
-    if start==end:
+    if start==len(nums)-1:
         return True
     
     else:
-        return nums[start] <= nums [start+1] and is_sorted(start+1,end,nums)
+        return nums[start] <= nums [start+1] and is_sorted(start+1,nums)
 
-print(is_sorted(0,len(nums)-1,nums))
+print(is_sorted(0,nums))
